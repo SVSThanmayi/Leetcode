e215cf525535d513ef515ecc77f232a98478ed52
SVSThanmayi <svsthanmayi@gmail.com>
2025-11-28 22:07:01 +0530

    Time: 1 ms (53.46%), Space: 56.6 MB (91.16%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    var k=1;
+    for(var i=1;i<nums.length;i++)
+    {
+        if (nums[i]!=nums[k - 1]) 
+        {
+            nums[k]=nums[i];
+            k++;
+        }
+    }
+    return k;
+};
