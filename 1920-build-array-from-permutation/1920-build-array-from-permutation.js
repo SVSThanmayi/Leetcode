2ce84484f24c0af6ccfa7b88fbd7ce0fe128a4a9
SVSThanmayi <svsthanmayi@gmail.com>
2025-11-30 22:24:44 +0530

    Time: 2 ms (31.08%), Space: 59.3 MB (8.51%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+    var arr=[];
+    for(var i=0;i<nums.length;i++)
+    {
+        arr[i]=nums[nums[i]];
+    }
+    return arr;
+};
