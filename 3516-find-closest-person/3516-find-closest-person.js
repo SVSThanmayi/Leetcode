86d485ca0b3b8694de59bf9063cc6a9675fcc14e
SVSThanmayi <svsthanmayi@gmail.com>
2026-02-15 16:26:13 +0530

    Time: 0 ms (100%), Space: 57.1 MB (12.83%) - LeetHub

---
+++ 3516-find-closest-person/3516-find-closest-person.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {number}
+ */
+var findClosest = function(x, y, z) {
+    if(Math.abs(z-x)<Math.abs(z-y))
+    return 1;
+    else if(Math.abs(z-x)>Math.abs(z-y))
+    return 2;
+    else return 0;
+};
