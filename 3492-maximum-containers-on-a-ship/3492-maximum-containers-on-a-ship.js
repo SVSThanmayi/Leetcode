7439abcd8fcac38d04361c78061c956956ac07e2
SVSThanmayi <svsthanmayi@gmail.com>
2026-02-15 16:47:43 +0530

    Time: 0 ms (100%), Space: 56.4 MB (38.24%) - LeetHub

---
+++ 3492-maximum-containers-on-a-ship/3492-maximum-containers-on-a-ship.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @param {number} w
+ * @param {number} maxWeight
+ * @return {number}
+ */
+var maxContainers = function(n, w, maxWeight) {
+    if(n*n*w < maxWeight)
+    {
+        return n*n;
+    }
+    else
+    {
+        return Math.floor(maxWeight/w);
+    }
+};
