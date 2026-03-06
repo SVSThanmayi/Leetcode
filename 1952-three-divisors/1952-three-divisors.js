6ab466b9c6aeed99dbcf1bd70939f275fca037fe
SVSThanmayi <svsthanmayi@gmail.com>
2026-03-06 21:07:22 +0530

    Time: 0 ms (100%), Space: 53.4 MB (78.34%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isThree = function(n) {
+    var count=0;
+    for(var i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        count++;
+    }
+    if(count==3)
+    return true;
+    return false;
+};
