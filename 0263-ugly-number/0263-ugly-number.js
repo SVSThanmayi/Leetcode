a8c521b498bc3a871a2530d27b9e0b94d32806ec
SVSThanmayi <svsthanmayi@gmail.com>
2026-05-18 12:28:28 +0530

    Time: 1 ms (44.33%), Space: 55.9 MB (28.27%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function(n) {
+
+    if(n <= 0)
+    {
+        return false;
+    }
+
+    while(n % 2 == 0)
+    {
+        n = n / 2;
+    }
+
+    while(n % 3 == 0)
+    {
+        n = n / 3;
+    }
+
+    while(n % 5 == 0)
+    {
+        n = n / 5;
+    }
+
+    if(n == 1)
+    {
+        return true;
+    }
+
+    return false;
+};
