d02bad339d7b11bd672c8d4d5b3767755c1ab832
SVSThanmayi <svsthanmayi@gmail.com>
2026-08-10 20:25:51 +0530

    Time: 1 ms (88.48%), Space: 57.9 MB (34.38%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,18 @@
+var majorityElement = function(nums) {
+    var candidate = 0;
+    var count = 0;
+
+    for (var i = 0; i < nums.length; i++) {
+        if (count === 0) {
+            candidate = nums[i];
+        }
+
+        if (nums[i] === candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    return candidate;
+};
